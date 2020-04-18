960feb378d20889971d1ea391c709d7ab109b670
adiladiletov <nurlubek009>
2020-04-18 12:05:10 +0600

    connected multer to route Photos to process files

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,14 @@
+const path = require('path');
+
+const rootPath = __dirname;
+
 module.exports = {
-  database: 'mongodb://localhost/photoGallery',
-  options: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-  }
+    database: 'mongodb://localhost/photoGallery',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    },
+    rootPath,
+    uploadPath: path.join(rootPath, '/public/uploads/')
 };
--- backend/router/Photos.js
+++ backend/router/Photos.js
@@ -1,6 +1,21 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+const {nanoid} = require('nanoid');
+
+const config = require('../config');
+
+const storage = multer.diskStorage({
+    destination: (req,file, cb)=>{
+        cb(null, config.uploadPath)
+    },
+    filename: (req,file, cb)=>{
+        cb(null, nanoid() + path.extname(file.originalname))
+    }
+});
+const upload = multer({storage});
 
 
 module.exports = router;
