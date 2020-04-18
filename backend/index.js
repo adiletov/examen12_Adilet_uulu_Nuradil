e80571b278e693c6eeadcf31eb03975385beb3bc
adiladiletov <nurlubek009>
2020-04-18 11:37:14 +0600

    created a connection to mongo

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,19 @@ const app = express();
 const port = 8080;
 
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./config');
 
 app.use(cors());
 app.use(express.json());
 
 
-
-const run = async () =>{
-   app.listen(port)
+const run = async () => {
+    await mongoose.connect(config.database, config.options);
+    app.listen(port)
 };
 
-run().catch(e=> {
+run().catch(e => {
     console.error(e)
 });
